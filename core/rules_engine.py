1b4ffc635122b102e447d76d22d026b405a22d17
Thorsten Schnebeck <thorsten.schnebeck@gmx.net>
2026-01-31 02:21:21 +0100

    Modernize core/rules_engine.py: update header and verify standards

--- core/rules_engine.py
+++ core/rules_engine.py
@@ -2,7 +2,7 @@
 ------------------------------------------------------------------------------
 Project:        KPaperFlux
 File:           core/rules_engine.py
-Version:        1.2.0
+Version:        2.0.0
 Producer:       thorsten.schnebeck@gmx.net
 Generator:      Antigravity
 Description:    Unified Rule Engine for evaluating and applying tagging rules.
@@ -11,7 +11,10 @@ Description:    Unified Rule Engine for evaluating and applying tagging rules.
 ------------------------------------------------------------------------------
 """
 
-from typing import List, Optional, Set
+from typing import TYPE_CHECKING, List, Optional, Set
+
+if TYPE_CHECKING:
+    from core.models.virtual import VirtualDocument
 
 from core.database import DatabaseManager
 from core.filter_tree import FilterNode, FilterTree
@@ -34,7 +37,7 @@ class RulesEngine:
         self.db: DatabaseManager = db
         self.filter_tree: FilterTree = filter_tree
 
-    def apply_rules_to_entity(self, v_doc: 'Document', rules: Optional[List[FilterNode]] = None, only_auto: bool = True) -> bool:
+    def apply_rules_to_entity(self, v_doc: 'VirtualDocument', rules: Optional[List[FilterNode]] = None, only_auto: bool = True) -> bool:
         """
         Evaluate and apply all enabled rules from the FilterTree to a single VirtualDocument.
 
