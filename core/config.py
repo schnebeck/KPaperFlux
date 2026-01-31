f35bc5e846ef6324f1765ee2f2301115f83b2411
Thorsten Schnebeck <thorsten.schnebeck@gmx.net>
2026-01-31 02:22:26 +0100

    Modernize core/config.py: update header

--- core/config.py
+++ core/config.py
@@ -2,7 +2,7 @@
 ------------------------------------------------------------------------------
 Project:        KPaperFlux
 File:           core/config.py
-Version:        1.2.0
+Version:        2.0.0
 Producer:       thorsten.schnebeck@gmx.net
 Generator:      Antigravity
 Description:    Manages application configuration using QSettings. Standardizes
